3acd4d7a2826bef0f316a3eb117dc01cb2813f04
andreluismoraes <andreluismoraes500@gmail.com>
2020-09-16 22:43:15 -0300

    Testando Calendário super customizado - estudos de funcionamento

--- App.js
+++ App.js
@@ -1,21 +1,12 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useEffect, useState } from "react";
+import { View, Text } from "react-native";
+
+import Agenda from "./src/Agenda";
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+    <View style={{ flex: 1 }}>
+      <Agenda />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
